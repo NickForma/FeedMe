a3d860a9a7ff0f8d38b4c223745c5ce4804eda8c
NickForma <nickforma60@gmail.com>
2019-09-27 17:05:10 -0400

    hbs homepage

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,16 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/funny", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("home", {
+        title: "MealPrepper",
+        msg: "HELLO GOATS",
+        examples: dbExamples
+      });
+    });
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
