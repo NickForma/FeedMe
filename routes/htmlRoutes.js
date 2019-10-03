1d1eb0be0d9cd78ea599bdf57f72060d9aca6833
Poonam Halani <halanipoonam@gmail.com>
2019-10-02 21:20:45 -0400

    Created home page .

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,19 +5,15 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
-        msg: "Welcome!",
+        msg: "FEED ME",
         examples: dbExamples
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/groceryList", function(req, res) {
+    res.render("groceryList");
   });
 
   // Render 404 page for any unmatched routes
