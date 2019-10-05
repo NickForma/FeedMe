34807a903c5b29e39a526b8e993f6eeef4f7f41b
SeeratSidhu <50206940+SeeratSidhu@users.noreply.github.com>
2019-10-05 10:13:07 -0400

    Search feature (#11)
    * meal planning page
    
    * changed to config
    
    * model
    
    * meal planning page
    
    * meal planning and ingredients list
    
    * meal plan page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,27 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
+  // app.get("/", function (req, res) {
+  //   db.Example.findAll({}).then(function (dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
+  app.get("/mealPlan", function(req, res){
+    res.render("mealPlan");
+  });
+
+  app.get("/ingredients/:id", function(req, res) {
+    db.Recipe.findOne({ where: { id: req.params.id } }).then(function(recipe) {
+      console.log(recipe.dataValues.recipeID);
+      res.render("ingredients", {
+        recipe: recipe
+      });
+    });
+  });
   app.get("/", function(req, res) {
     db.Recipe.findAll({}).then(function(dbExamples) {
       res.render("index", {
@@ -18,6 +39,24 @@ module.exports = function(app) {
       });
     });
   });
+  // app.get("/mealPlan", function(req, res) {
+  //   res.render("mealPlan");
+  // });
+
+  // app.get("/ingredients/:id", function(req, res) {
+  //   db.Recipe.findOne({ where: { id: req.params.id } }).then(function() {
+  //     res.render("ingredients");
+  //   });
+  // });
+
+  // Load example page and pass in an example by id
+  // app.get("/example/:id", function (req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Load example page and pass in an example by id
   app.get("/groceryList", function(req, res) {
