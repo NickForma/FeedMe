c9f1b7a616adc134fdf92df190794edeeb8412fd
NickForma <nickforma60@gmail.com>
2019-10-05 10:36:07 -0400

    herku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   //   });
   // });
 
-  app.get("/mealPlan", function(req, res){
+  app.get("/mealPlan", function(req, res) {
     res.render("mealPlan");
   });
 
